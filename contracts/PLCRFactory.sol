1ba34885080c352445e85ed105509f546d26d3a6
Emily Williams <emily.williams@levelk.io>
2018-10-15 14:37:11 -0400

    Use zeppelin tokens in PLCRFactory

--- contracts/PLCRFactory.sol
+++ contracts/PLCRFactory.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.4.20;
 
-import "tokens/eip20/EIP20.sol";
+import "./ERC20Token.sol";
 import "./PLCRVoting.sol";
 import "./ProxyFactory.sol";
 
 contract PLCRFactory {
 
-  event newPLCR(address creator, EIP20 token, PLCRVoting plcr);
+  event newPLCR(address creator, ERC20Token token, PLCRVoting plcr);
 
   ProxyFactory public proxyFactory;
   PLCRVoting public canonizedPLCR;
@@ -20,9 +20,9 @@ contract PLCRFactory {
   /*
   @dev deploys and initializes a new PLCRVoting contract that consumes a token at an address
   supplied by the user.
-  @param _token an EIP20 token to be consumed by the new PLCR contract
+  @param _token an ERC20Token token to be consumed by the new PLCR contract
   */
-  function newPLCRBYOToken(EIP20 _token) public returns (PLCRVoting) {
+  function newPLCRBYOToken(ERC20Token _token) public returns (PLCRVoting) {
     PLCRVoting plcr = PLCRVoting(proxyFactory.createProxy(canonizedPLCR, ""));
     plcr.initialize(_token);
 
@@ -32,12 +32,12 @@ contract PLCRFactory {
   }
 
   /*
-  @dev deploys and initializes a new PLCRVoting contract and an EIP20 to be consumed by the PLCR's
+  @dev deploys and initializes a new PLCRVoting contract and an ERC20Token to be consumed by the PLCR's
   initializer.
-  @param _supply the total number of tokens to mint in the EIP20 contract
-  @param _name the name of the new EIP20 token
-  @param _decimals the decimal precision to be used in rendering balances in the EIP20 token
-  @param _symbol the symbol of the new EIP20 token
+  @param _supply the total number of tokens to mint in the ERC20Token contract
+  @param _name the name of the new ERC20Token token
+  @param _decimals the decimal precision to be used in rendering balances in the ERC20Token token
+  @param _symbol the symbol of the new ERC20Token token
   */
   function newPLCRWithToken(
     uint _supply,
@@ -46,7 +46,7 @@ contract PLCRFactory {
     string _symbol
   ) public returns (PLCRVoting) {
     // Create a new token and give all the tokens to the PLCR creator
-    EIP20 token = new EIP20(_supply, _name, _decimals, _symbol);
+    ERC20Token token = new ERC20Token(_supply, _name, _decimals, _symbol);
     token.transfer(msg.sender, _supply);
 
     // Create and initialize a new PLCR contract
