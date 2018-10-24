7f459f5b3ef9d3860bdb78b25b78fc3d39de3e73
Mike Calvanese <mfcalvanese@gmail.com>
2018-10-24 17:58:47 -0400

    remove commented function from PLCRFactory

---
+++ contracts/Imports.sol
@@ -0,0 +1,3 @@
+pragma solidity ^0.4.20;
+
+import "openzeppelin-eth/contracts/token/ERC20/StandaloneERC20.sol";
--- contracts/PLCRFactory.sol
+++ contracts/PLCRFactory.sol
@@ -29,32 +29,4 @@ contract PLCRFactory {
 
     return plcr;
   }
-
-  // /*
-  // @dev deploys and initializes a new PLCRVoting contract and a StandaloneERC20 to be consumed by the PLCR's
-  // initializer.
-  // @param _supply the total number of tokens to mint in the StandaloneERC20
-  // @param _name the name of the new StandaloneERC20
-  // @param _decimals the decimal precision to be used in rendering balances in the StandaloneERC20
-  // @param _symbol the symbol of the new StandaloneERC20
-  // */
-  // function newPLCRWithToken(
-  //   uint _supply,
-  //   string _name,
-  //   uint8 _decimals,
-  //   string _symbol
-  // ) public returns (PLCRVoting) {
-  //   // Create a new token and give all the tokens to the PLCR creator
-  //   // StandaloneERC20 token = new StandaloneERC20();
-  //   // token.initialize(msg.sender);
-  //   // token.mint(msg.sender, _supply);
-
-  //   // // Create and initialize a new PLCR contract
-  //   // PLCRVoting plcr = PLCRVoting(proxyFactory.createProxy(canonizedPLCR, ""));
-  //   // plcr.initialize(token);
-
-  //   // emit newPLCR(msg.sender, token, plcr);
-
-  //   // return plcr;
-  // }
 }
